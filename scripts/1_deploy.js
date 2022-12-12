70be33301ce48e92d9d463250a397df3d795ba93
dangkhuong14 <f13file31@gmail.com>
2022-12-12 12:11:12 +0700

    Test contract with hardhat console and deploy to local node

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -4,23 +4,17 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+// const hre = require("hardhat");
+// const ethers = require("ethers")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    //Fetch contract 
+//   const Token = await hre.ethers.getContractFactory("Token")
+    const Token = await ethers.getContractFactory("Token")
+  //Deploy the contract
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log(`The contract is deployed at address: ${token.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
