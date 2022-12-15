8995e00b6a8f367a16f0c53e4e0c779dcb2d2323
dangkhuong14 <f13file31@gmail.com>
2022-12-15 22:15:58 +0700

    Add second deploy file for seed exchange

---
+++ scripts/2_seed-exchange.js
@@ -0,0 +1,19 @@
+async function main() {
+    //Distribute tokens
+
+    //Deposit tokens to exchange
+
+    //Make orders
+
+    //Cancel orders
+
+    //Fill orders
+    
+} 
+
+main()
+    .then(()=>process.exit(0))
+    .catch((error)=>{
+        console.error(error);
+        process.exit(1);
+    })
