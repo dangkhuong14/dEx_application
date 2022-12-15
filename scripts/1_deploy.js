301c7d432c10278202abf02654ff3b5e5e27195a
dangkhuong14 <f13file31@gmail.com>
2022-12-15 22:04:31 +0700

    Deploy token contracts and exchange contract to local node

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,24 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-// const hre = require("hardhat");
-// const ethers = require("ethers")
-
 async function main() {
-    //Fetch contract 
-    //   const Token = await hre.ethers.getContractFactory("Token")
-    const Token = await ethers.getContractFactory("Token")
+  console.log('Preparing deployment...');
+  
+  //Fetch contract to deploy
+  const Token = await hre.ethers.getContractFactory('Token')
+  const Exchange = await hre.ethers.getContractFactory('Exchange')
+
+  //Fetch accounts
+  const accounts = await ethers.getSigners()
+
+  console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}`);
+
   //Deploy the contract
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log(`The contract is deployed at address: ${token.address}`);
+  const dapp = await Token.deploy('Dapp token', 'DAPP', 1000000);
+  await dapp.deployed();
+  console.log(`DAPP is deployed at address: ${dapp.address}`);
+
+  const mETH = await Token.deploy('mETH', 'mETH', 1000000);
+  await mETH.deployed();
+  console.log(`mETH is deployed at address: ${mETH.address}`);
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', 1000000);
+  await mDAI.deployed();
+  console.log(`mDAI is deployed at address: ${mDAI.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange deployed to: ${exchange.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
