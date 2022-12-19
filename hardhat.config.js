faa8b1269187b836aa5d9cac28354e2af5b7ffc2
dangkhuong14 <f13file31@gmail.com>
2022-12-19 09:12:29 +0700

    Set up live network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,25 @@
+const { slice } = require("lodash");
+
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privatekeys = process.env.PRIVATE_KEYS || ''
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   network: {
-    localhost:{}
+    localhost:{},
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privatekeys.split(',')
+    },
+    mumbai: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privatekeys.split(',')
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privatekeys.split(",")
+    }
   }
 };
